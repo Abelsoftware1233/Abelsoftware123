d0a72e17bbd9892e7dcc1b8343edab9132f5d66d
Abelsoftware123 <abelsoftware123@hotmail.com>
2025-10-26 16:18:34 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-document.getElementById('contact-knop').addEventListener('click', function() {
-    alert('Bedankt voor uw interesse! Stuur een e-mail naar abelsoftware123@hotmail.com.');
+Document.getElementById('contact-knop').addEventListener('click', function() {
+    alert('Thank you for your interest! Send an email to abelsoftware123@hotmail.com.');
 });
