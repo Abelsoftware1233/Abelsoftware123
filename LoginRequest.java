5fda97ac7432a08630d2f246c4e74996efa8af8f
Abelsoftware123 <abelsoftware123@hotmail.com>
2025-11-19 19:07:49 +0100

    Update LoginRequest.java

--- LoginRequest.java
+++ LoginRequest.java
@@ -1,25 +1,13 @@
 // LoginRequest.java
-package com.abelsoftware123.registration.controller;
+package com.abelsoftware123.registratie.dto;
 
 public class LoginRequest {
-    // Variabelen moeten overeenkomen met de JSON keys: username en password
-    private String username;
+    private String usernameOrEmail;
     private String password;
 
-    // Zorg ervoor dat de Getters en Setters aanwezig zijn
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
+    // Getters en Setters
+    public String getUsernameOrEmail() { return usernameOrEmail; }
+    public void setUsernameOrEmail(String usernameOrEmail) { this.usernameOrEmail = usernameOrEmail; }
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
 }
