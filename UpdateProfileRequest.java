abbc163d77fd4c2eb36183e6b9d26eaa6db76a62
Abelsoftware123 <abelsoftware123@hotmail.com>
2025-11-19 19:12:57 +0100

    Update UpdateProfileRequest.java

--- UpdateProfileRequest.java
+++ UpdateProfileRequest.java
@@ -5,16 +5,39 @@ public class UpdateProfileRequest {
     private String email;
     private String firstName;
     private String lastName;
-    private String newPassword; 
+    private String newPassword; // Optioneel: alleen gevuld als het wachtwoord wordt gewijzigd
 
     // Getters en Setters
 
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
-    public String getFirstName() { return firstName; }
-    public void setFirstName(String firstName) { this.firstName = firstName; }
-    public String getLastName() { return lastName; }
-    public void setLastName(String lastName) { this.lastName = lastName; }
-    public String getNewPassword() { return newPassword; }
-    public void setNewPassword(String newPassword) { this.newPassword = newPassword; }
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getNewPassword() {
+        return newPassword;
+    }
+
+    public void setNewPassword(String newPassword) {
+        this.newPassword = newPassword;
+    }
 }
