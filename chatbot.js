da1389a696b3f0900fcb1a000fa7b802d3e24992
Abelsoftware123 <abelsoftware123@hotmail.com>
2026-02-14 22:16:11 +0100

    Update chatbot.js

--- chatbot.js
+++ chatbot.js
@@ -61,7 +61,13 @@ const echoBotData = {
             en: "We keep it fair: Games start at €4.99, and AI software from €15. Quality made with love, for a fair price! 💸",
             nl: "We houden het eerlijk: Games vanaf €4,99 en AI-software vanaf €15. Kwaliteit gemaakt met liefde, voor een eerlijke prijs! 💸"
         },
-        "prices": "prijzen", "pricing": "prijzen", "kosten": "prijzen",
+"prices": "prijzen", "pricing": "prijzen", "kosten": "prijzen",
+
+       "contact" : {
+            en: "do you want human contact send us Email on abelsoftware123@hotmail.com and we will help you within 24 hours! 💻",
+            nl: "Wilt u Menselijk contact stuur ons een email op abelsoftware123@hotmail.com en we reageren binnen 24uur om u te helpen! 💻"
+        },
+        "contact": "kontact", "customs": "klantenservice", "customer": "company",
 
         // --- 4. AI SOFTWARE & ABELSOFTWARE123 APP ---
         "ai software": {
