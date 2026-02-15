b460364306f3f8b2fde0662d0d1d567aac575591
Abelsoftware123 <abelsoftware123@hotmail.com>
2026-02-15 15:00:50 +0100

    Update SecurityConfig.java

--- SecurityConfig.java
+++ SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.jouwgamedomein.registratie.config;
+package com.abelsoftware123.registratie.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
