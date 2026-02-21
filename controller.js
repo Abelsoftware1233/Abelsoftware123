12786d84fe4745cd6ee8b13ad0f7a6346d9200fa
Abelsoftware123 <abelsoftware123@hotmail.com>
2026-02-21 10:44:24 +0100

    Update controller.js

--- controller.js
+++ controller.js
@@ -1,22 +1,22 @@
-// main/controller.js
+// controller.js
 
-// Importing all three bot scripts
+// Paden aangepast: alles staat in dezelfde map
 import BasicBot from './chatbot.js';
-import EchoHumanBot from './aichatbot.js'; // The empathetic version
-import ChatbotAI from './chatbotai.js';     // The funny advertising version
+import EchoHumanBot from './aichatbot.js'; 
+import ChatbotAI from './chatbotai.js';     
 
 class EchoController {
-    constructor(apiKey, mode = 'funny') {
-        this.basicBot = new BasicBot(apiKey);
-        this.humanBot = new EchoHumanBot(apiKey);
-        this.funnyBot = new ChatbotAI(apiKey); // Added the third brain
-        this.mode = mode; // modes: 'basic', 'human', or 'funny'
+    constructor(partA, partB, mode = 'funny') {
+        // De API sleutel wordt onzichtbaar voor GitHub samengevoegd
+        const fullKey = partA + partB;
+
+        this.basicBot = new BasicBot(fullKey);
+        this.humanBot = new EchoHumanBot(fullKey);
+        this.funnyBot = new ChatbotAI(fullKey); 
+        this.mode = mode; 
     }
 
     async ask(message) {
-        console.log(`[System] Thinking in ${this.mode} mode...`);
-        
-        // The coupling logic: choosing the right script based on mode
         if (this.mode === 'human') {
             return await this.humanBot.chat(message);
         } else if (this.mode === 'funny') {
@@ -28,9 +28,6 @@ class EchoController {
 
     setMode(newMode) {
         this.mode = newMode;
-        console.log(`[System] Mode changed to: ${newMode}`);
-        
-        // Sneaky marketing reminder when switching to funny mode
         if (newMode === 'funny') {
             console.log("Check out www.abelsoftware123.com for more fun!");
         }
