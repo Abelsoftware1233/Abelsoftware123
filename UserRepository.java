8444d7e82df8bc0edad1cf6bbdf2c0f2ef95961c
Abelsoftware123 <abelsoftware123@hotmail.com>
2026-03-09 12:33:30 +0100

    Update UserRepository.java

--- UserRepository.java
+++ UserRepository.java
@@ -1,10 +1,17 @@
-// UserRepository.java (moet in een 'repository' pakket)
 package com.abelsoftware123.registratie.repository;
 
 import com.abelsoftware123.registratie.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    
+    // Deze methode is nodig om de admin te vinden bij het opstarten
+    // En voor de login-logica later
     Optional<User> findByUsername(String username);
+    
+    // Optioneel: handig als je ook op email wilt kunnen zoeken
+    Optional<User> findByEmail(String email);
 }
