abe51e8fb772f4308edb6e2377712a295064246f
Abelsoftware123 <abelsoftware123@hotmail.com>
2026-03-09 12:45:07 +0100

    Update AdminController.java

--- AdminController.java
+++ AdminController.java
@@ -1,20 +1,40 @@
+package com.abelsoftware123.registratie.controller;
+
+import com.abelsoftware123.registratie.model.User;
+import com.abelsoftware123.registratie.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/admin")
 public class AdminController {
 
-    private final UserRepository userRepository;
+    @Autowired
+    private UserRepository userRepository;
 
-    public AdminController(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
-
+    // 1. Haal alle gebruikers op voor de tabel in admin.html
     @GetMapping("/users")
-    public List<User> getAllUsers() {
-        return userRepository.findAll();
+    public ResponseEntity<List<User>> getAllUsers() {
+        return ResponseEntity.ok(userRepository.findAll());
     }
-    
+
+    // 2. Verwijder een gebruiker
     @DeleteMapping("/users/{id}")
-    public void deleteUser(@PathVariable Long id) {
+    public ResponseEntity<?> deleteUser(@PathVariable Long id) {
         userRepository.deleteById(id);
+        return ResponseEntity.ok().build();
+    }
+
+    // 3. Maak een gebruiker Admin (Extra functie!)
+    @PutMapping("/users/{id}/make-admin")
+    public ResponseEntity<?> makeAdmin(@PathVariable Long id) {
+        return userRepository.findById(id).map(user -> {
+            user.setRole("ROLE_ADMIN");
+            userRepository.save(user);
+            return ResponseEntity.ok().build();
+        }).orElse(ResponseEntity.notFound().build());
     }
 }
