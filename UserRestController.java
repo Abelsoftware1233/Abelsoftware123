487b247602ac52999156b9a3e796f51d360f5901
Abelsoftware123 <abelsoftware123@hotmail.com>
2026-03-10 18:53:11 +0100

    Create UserRestController.java

---
+++ UserRestController.java
@@ -0,0 +1,30 @@
+package com.abelsoftware123.registratie.controller;
+
+import com.abelsoftware123.registratie.model.User; // Zorg dat de import klopt met jouw project
+import com.abelsoftware123.registratie.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserRestController {
+
+    private final UserService userService;
+
+    public UserRestController(UserService userService) {
+        this.userService = userService;
+    }
+
+    // Dit stuurt de lijst met gebruikers naar je admin.js fetch()
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.findAllUsers(); // Zorg dat deze methode in je UserService staat
+    }
+
+    // Dit laat je gebruikers verwijderen via de knop in de tabel
+    @DeleteMapping("/{id}")
+    public void deleteUser(@往PathVariable Long id) {
+        userService.deleteUserById(id);
+    }
+}
