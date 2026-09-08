978c36a2d79bcdf40a2c03df259b0d9d4ce45f81
A.A <abelsoftware123@hotmail.com>
2026-09-08 16:36:20 +0200

    Update UserController.java

--- UserController.java
+++ UserController.java
@@ -2,6 +2,7 @@ package com.abelsoftware123.registratie;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
