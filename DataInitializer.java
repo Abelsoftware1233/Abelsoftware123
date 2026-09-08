6563257c2bbeaf66042c88c7cf3aaa15b9af2827
A.A <abelsoftware123@hotmail.com>
2026-09-08 16:34:02 +0200

    Update DataInitializer.java

--- DataInitializer.java
+++ DataInitializer.java
@@ -1,7 +1,5 @@
-package com.abelsoftware123.registratie.config;
+package com.abelsoftware123.registratie;
 
-import com.abelsoftware123.registratie.model.User;
-import com.abelsoftware123.registratie.repository.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,12 +11,10 @@ public class DataInitializer {
     @Bean
     CommandLineRunner initDatabase(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         return args -> {
-            // Check of de admin al bestaat om dubbele accounts te voorkomen
             if (userRepository.findByUsername("admin").isEmpty()) {
                 User admin = new User();
                 admin.setUsername("admin");
                 admin.setEmail("admin@abelsoftware123.com");
-                // We hashen het wachtwoord direct hier
                 admin.setPasswordHash(passwordEncoder.encode("admin123"));
                 admin.setRole("ROLE_ADMIN");
                 admin.setFirstName("abel");
