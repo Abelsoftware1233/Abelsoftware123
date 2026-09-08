724e8c2412b7b41df779084ad232ce59f9b5c1e8
A.A <abelsoftware123@hotmail.com>
2026-09-08 16:16:15 +0200

    Create RegistratieApplication.java

---
+++ RegistratieApplication.java
@@ -0,0 +1,13 @@
+package com.abelsoftware123.registratie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistratieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegistratieApplication.class, args);
+    }
+
+}
