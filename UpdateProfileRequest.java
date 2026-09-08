9901cb2eb4f0e782fa0936dbebb4a83858c5e67d
A.A <abelsoftware123@hotmail.com>
2026-09-08 16:25:35 +0200

    Update UpdateProfileRequest.java

--- UpdateProfileRequest.java
+++ UpdateProfileRequest.java
@@ -1,43 +1,20 @@
-// UpdateProfileRequest.java
-package com.abelsoftware123.registratie.dto;
+package com.abelsoftware123.registratie;
 
 public class UpdateProfileRequest {
     private String email;
     private String firstName;
     private String lastName;
-    private String newPassword; // Optioneel: alleen gevuld als het wachtwoord wordt gewijzigd
+    private String newPassword;
 
-    // Getters en Setters
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
 
-    public String getEmail() {
-        return email;
-    }
+    public String getFirstName() { return firstName; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+    public String getLastName() { return lastName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
 
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getNewPassword() {
-        return newPassword;
-    }
-
-    public void setNewPassword(String newPassword) {
-        this.newPassword = newPassword;
-    }
+    public String getNewPassword() { return newPassword; }
+    public void setNewPassword(String newPassword) { this.newPassword = newPassword; }
 }
